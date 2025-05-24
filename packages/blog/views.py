649d627eb7f2ffb22e18db6404fd3aab289ea74a
esangayt <24_de_7@protonmail.com>
2025-05-23 21:01:21 -0500

    featL Handling forms in views

--- packages/blog/views.py
+++ packages/blog/views.py
@@ -3,8 +3,32 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
+from .forms import EmailPostForm
 from .models import Post
 
+
+def post_share(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status = Post.Status.PUBLISHED
+    )
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+
+    return render(
+        request,
+        'blog/post/share.html',
+        {
+            'post': post,
+            'form': form,
+        }
+    )
 
 def post_list(request):
     post_list = Post.published.all()
