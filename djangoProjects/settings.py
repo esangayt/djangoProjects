3343299f80acfb98f2a0c6f5b1010472bfd74fd7
esangayt <24_de_7@protonmail.com>
2025-06-22 22:07:27 -0500

    chore: Configuración de Email

--- djangoProjects/settings.py
+++ djangoProjects/settings.py
@@ -88,6 +88,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
